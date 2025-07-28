c43534edb1a075d7532f5a1f6f73fd161ac6cc5a
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-07-28 14:31:52 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My Quickshell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs"; # THIS IS IMPORTANT
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default =
+        pkgs.mkShell
+        {
+          packages = with pkgs; [
+            kdePackages.qtdeclarative
+            quickshell.packages.${pkgs.system}.default
+          ];
+          shellHook = ''
+            echo "Generating empty .qmlls.ini file if non-existent"
+            touch .qmlls.ini
+          '';
+        };
+    });
+  };
+}
